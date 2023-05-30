ef745e946d501f0e257676b0d0b08ca219b54209
Oscar Hinojosa <125847665+Oscar135-33@users.noreply.github.com>
2023-05-30 13:11:12 -0600

    agregandole-JS-producto
    se agrego funcionalidad a la pag producto

--- producto.js
+++ producto.js
@@ -1,37 +1,45 @@
+// Obtener elementos del DOM
+const btnIncrement = document.getElementById('increment');
+const btnDecrement = document.getElementById('decrement');
 const inputQuantity = document.querySelector('.input-quantity');
-const btnIncrement = document.querySelector('#increment');
-const btnDecrement = document.querySelector('#decrement');
+const toggleDescription = document.querySelector('.title-description');
+const contentDescription = document.querySelector('.text-description');
+const btnAddToCart = document.querySelector('.btn-add-to-cart');
+const selectSize = document.getElementById('size');
+const btnClean = document.querySelector('.btnclean');
 
-let valueByDefault = parseInt(inputQuantity.value);
-
-// Funciones Click
-
+// Manejador de eventos para el botón de incremento
 btnIncrement.addEventListener('click', () => {
-	valueByDefault += 1;
-	inputQuantity.value = valueByDefault;
+  let value = parseInt(inputQuantity.value);
+  value++;
+  inputQuantity.value = value;
 });
 
+// Manejador de eventos para el botón de decremento
 btnDecrement.addEventListener('click', () => {
-	if (valueByDefault === 1) {
-		return;
-	}
-	valueByDefault -= 1;
-	inputQuantity.value = valueByDefault;
+  let value = parseInt(inputQuantity.value);
+  if (value > 1) {
+    value--;
+    inputQuantity.value = value;
+  }
 });
 
-// Toggle
-// Constantes Toggle Titles
-const toggleDescription = document.querySelector(
-	'.title-description'
-);
-
-
-// Constantes Contenido Texto
-const contentDescription = document.querySelector(
-	'.text-description'
-);
-
-// Funciones Toggle
+// Manejador de eventos para el botón de descripción
 toggleDescription.addEventListener('click', () => {
-	contentDescription.classList.toggle('hidden');
+  contentDescription.classList.toggle('hidden');
+});
+
+// Manejador de eventos para el botón "Añadir al carrito"
+btnAddToCart.addEventListener('click', () => {
+  const size = selectSize.value;
+  const quantity = parseInt(inputQuantity.value);
+  
+  // Lógica para añadir al carrito
+  console.log(`Añadido al carrito - Talla: ${size}, Cantidad: ${quantity}`);
+});
+
+// Manejador de eventos para el botón "Limpiar"
+btnClean.addEventListener('click', () => {
+  inputQuantity.value = '1';
+  selectSize.selectedIndex = 0;
 });
