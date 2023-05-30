974a752479edc2f155b239214e553e21d3b73189
Guillermo V. Flores Báez <guillermo.floresb@uanl.edu.mx>
2023-05-30 09:26:38 -0600

    Agreagación de la página de producto
    Se realizó parte de la página de HTML, CSS y el funcionalidad de JS del aparatado de Producto

---
+++ producto.js
@@ -0,0 +1,37 @@
+const inputQuantity = document.querySelector('.input-quantity');
+const btnIncrement = document.querySelector('#increment');
+const btnDecrement = document.querySelector('#decrement');
+
+let valueByDefault = parseInt(inputQuantity.value);
+
+// Funciones Click
+
+btnIncrement.addEventListener('click', () => {
+	valueByDefault += 1;
+	inputQuantity.value = valueByDefault;
+});
+
+btnDecrement.addEventListener('click', () => {
+	if (valueByDefault === 1) {
+		return;
+	}
+	valueByDefault -= 1;
+	inputQuantity.value = valueByDefault;
+});
+
+// Toggle
+// Constantes Toggle Titles
+const toggleDescription = document.querySelector(
+	'.title-description'
+);
+
+
+// Constantes Contenido Texto
+const contentDescription = document.querySelector(
+	'.text-description'
+);
+
+// Funciones Toggle
+toggleDescription.addEventListener('click', () => {
+	contentDescription.classList.toggle('hidden');
+});
